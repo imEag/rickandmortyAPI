751ea9ba37aba8fd7f5d9ad542d27d9006b85e15
Emmanuel <emma145@hotmail.es>
2022-07-30 17:36:35 -0500

    add cros and headers setup

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ app.use('/graphql', expressGraphQL({
     graphiql: true
 }));
 
+
+// headers and cors setup
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // WEBPACK
 /* const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
